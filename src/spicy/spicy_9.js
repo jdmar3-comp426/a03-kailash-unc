9f4a10a91beaea326ceafd7227ff3829574b8f66
kailash-unc <kailash@email.unc.edu>
2021-11-15 01:53:28 -0500

    Are we done done

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -170,7 +170,7 @@ export const filter = (arr, test) => {
  *   odd numbers. Use the "everyEven" function in this function.
  */
 export const allEvensAreOdd = (arr) => {
-
+    return everyEven(arr, x => x % 2 != 0);
 };
 
 
@@ -180,7 +180,7 @@ export const allEvensAreOdd = (arr) => {
  *   array is an odd number. Use the "someEven" function in this function.
  */
 export const anEvenIsOdd = (arr) => {
-
+    return someEven(arr, x => x % 2 != 0);
 };
 
 
@@ -191,5 +191,10 @@ export const anEvenIsOdd = (arr) => {
  *   pass the test. You must use the filter function.
  */
 export const hasExactly = (arr, test, n) => {
-
+    for(let i = 0; i < arr; i++){
+        if(test(arr[i])){
+            n--;
+        }
+    }
+    return n == 0
 };
