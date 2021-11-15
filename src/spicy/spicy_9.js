3b0f835f5f8de2cd8e141226da24d28ca415adf2
kailash-unc <kailash@email.unc.edu>
2021-11-15 00:48:31 -0500

    test spi

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -45,7 +45,9 @@ export const repeatDemo = () => {
  *   product of num1 and num2.
  */
 export const multiplyBy = (num1) => {
-
+    return function(num2){
+        return num1 * num2;
+    }
 };
 
 
